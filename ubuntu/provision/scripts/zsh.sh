26ed0df6a09988c07d998ac873378b95071ce660
Simon Lepel <simbo@simbo.de>
2016-07-16 22:26:47 +0200

    remove zsh-git-prompt from provisioning

--- ubuntu/provision/scripts/zsh.sh
+++ ubuntu/provision/scripts/zsh.sh
@@ -13,10 +13,6 @@ echo_c "Installing oh-my-zsh..."
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git $VAGRANT_HOME/.oh-my-zsh
 
-echo_c "Installing zsh-git-prompt..."
-
-git clone git://github.com/olivierverdier/zsh-git-prompt.git $VAGRANT_HOME/.zsh-git-prompt
-
 echo_c "Copying zsh config..."
 
 cp $PROVISION_FILES/.zsh* $VAGRANT_HOME/.
